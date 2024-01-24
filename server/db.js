fa7061b62ed6824afa8a096d53856839eb615cc8
Andrei Frandes <131894678+AndreiWTP2000@users.noreply.github.com>
2024-01-24 03:37:54 +0000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'todoapp_6d2w_user',
   password: 'qFxQSfzrjGYyhuCTdMmATLDDEZsxc9LK',
-  host: 'dpg-cmo6ha5a73kc73b4etjg-a.ohio-postgres.render.com', // Correct hostname
+  host: 'qFxQSfzrjGYyhuCTdMmATLDDEZsxc9LK@dpg-cmo6ha5a73kc73b4etjg-a.ohio-postgres.render.com', // Correct hostname
   database: 'todoapp_6d2w',
   port: 5432, // or your specific port
 });
