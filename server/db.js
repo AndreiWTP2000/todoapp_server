0ad2f43a5e5cb453e5a15abeaab96d7388fd2524
Andrei Frandes <131894678+AndreiWTP2000@users.noreply.github.com>
2024-05-09 21:01:34 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'todoapp_g23f_user',
-  password: 'YW9q0dCp3Ki10FchoWcZzzOueQNJICYr',
-  host: 'dpg-cmo925gl5elc738rfipg-a', // Correct hostname
-  database: 'todoapp_g23f',
+  user: 'todoapp_vt25_user',
+  password: 'vRSiDyOoqfuK03KbZAzxO5iM34zsfOCv',
+  host: 'dpg-couid1n79t8c739i6pv0-a', // Correct hostname
+  database: 'todoapp_vt25',
   port: 5432, // or your specific port
 });
 module.exports = pool;
